c107e7ed0da09ce236c3b26db7aefa15468c03df
keple <smw195@gmail.com>
2021-12-21 00:11:47 +0900

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,77 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <md-app>
+    <md-app-toolbar class="md-primary">
+      <span class="md-title">My Title</span>
+    </md-app-toolbar>
+
+    <md-app-drawer md-permanent="full">
+      <md-toolbar class="md-transparent" md-elevation="0">
+        Navigation
+      </md-toolbar>
+
+      <md-list>
+        <md-list-item>
+          <md-icon>move_to_inbox</md-icon>
+          <span class="md-list-item-text">Inbox</span>
+        </md-list-item>
+
+        <md-list-item>
+          <md-icon>send</md-icon>
+          <span class="md-list-item-text">Sent Mail</span>
+        </md-list-item>
+
+        <md-list-item>
+          <md-icon>delete</md-icon>
+          <span class="md-list-item-text">Trash</span>
+        </md-list-item>
+
+        <md-list-item>
+          <md-icon>error</md-icon>
+          <span class="md-list-item-text">Spam</span>
+        </md-list-item>
+      </md-list>
+    </md-app-drawer>
+
+    <md-app-content>
+
+    </md-app-content>
+  </md-app>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default-dark.css'
+import VueMaterial from 'vue-material'
 
 export default {
+  install : (app , options) => {
+
+    app.provide(VueMaterial , options);
+  },
+
   name: 'App',
+
   components: {
-    HelloWorld
+
   }
 }
 </script>
 
 <style>
+  @import "./assets/globalCss/globalCss.css";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+}
+#logo {
+  width: 70px;
+  height:60px;
+  
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import {createRouter , createWebHistory} from 'vue-router@next';
 createApp(App).mount('#app')
+
+const routes = [{
+    path : '/',
+    name : 'home',
+    component : () => import('@/view/home')
+    }
+]
+
+const router = createRouter ({
+    history : createWebHistory(),
+    routes,
+})
+
+export default router;
