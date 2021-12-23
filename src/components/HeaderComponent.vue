301d44f83237febeffcfc7c1c90f92e7070758e7
keple <smw195@gmail.com>
2021-12-23 22:38:39 +0900

    profile dropdown

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -4,14 +4,25 @@
       <div class="w-25 h-100">
         ??
       </div>
-      <div class="d-flex justify-content-center align-items-center me-md-5">
-        <div id="avatar" class="d-flex justify-content-center bg-white m-2">
+      <div class="d-flex justify-content-center align-items-center me-md-5 pointer" >
+        <div id="avatar" class="d-flex justify-content-center bg-white m-2 " >
           <img id="avatar-img" class="w-100 h-100" src="@/assets/logo.png">
         </div>
         <div class="d-inline">
-          <span>avatar aria</span>
+          <li class="nav-item dropdown">
+            <a class="dropdown-toggle text-white" aria-haspopup="true" data-bs-toggle="dropdown" aria-expanded="false">avatar</a>
+            <ul class="dropdown-menu dropdown-menu-dark dropdown-menu-end text-white">
+              <li><a class="dropdown-item text-white" href="/profile">프로필수정</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li class="p-2">
+                <button class="btn btn-danger">로그아웃</button>
+              </li>
+            </ul>
+          </li>
         </div>
+
       </div>
+
     </div>
   </div>
 
